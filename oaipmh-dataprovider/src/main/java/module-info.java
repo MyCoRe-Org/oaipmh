a3ee6660fd980db441da55768504e2370c0f4575
Thomas Scheffler <github@yagee.de>
2022-04-28 14:03:16 +0200

    update dependencies

--- oaipmh-dataprovider/src/main/java/module-info.java
+++ oaipmh-dataprovider/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module org.mycore.oai.pmh.dataprovider {
     requires org.mycore.oai.pmh;
     requires org.apache.logging.log4j;
-    requires jdom2;
+    requires org.jdom2;
     exports org.mycore.oai.pmh.dataprovider;
 }
